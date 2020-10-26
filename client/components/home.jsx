0d90cbf37bdc83b9d1af54819b1b57c3bb1cd6f9
Cody Miller <cody.miller@learningfuze.com>
2020-10-25 21:15:08 -0700

    adjusted oauth route

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,36 +1,37 @@
 import React from 'react';
 
 class Home extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       data: null,
       isLoading: true
-    }
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getUserData();
   }
 
-  getUserData(){
-    fetch("https://api.github.com/user", {
+  getUserData() {
+    fetch('https://api.github.com/user', {
       headers: {
-        'Authorization': `token ${this.props.match.params.token}`
+        Authorization: `token ${this.props.match.params.token}`
       }
     })
-    .then(res => res.json())
-    .then(data => this.setState({
-      isLoading: false,
-      data
-    }, console.log))
+      .then(res => res.json())
+      .then(data => this.setState({
+        isLoading: false,
+        data
+      }, console.log));
   }
+
   render() {
     return (
       <div>
         <h1>Logged In!</h1>
         <h4>Access Token: {this.props.match.params.token}</h4>
-        <p>{this.state.isLoading ? "Loading..." : "Data retrieved!"}</p>
+        <p>{this.state.isLoading ? 'Loading...' : 'Data retrieved!'}</p>
       </div>
     );
   }
